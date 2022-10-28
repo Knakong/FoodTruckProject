cb85dd96b44c59599a22b4c9c4c7443b8cdfce67
Knakong <roathana.kong@gmail.com>
2022-10-28 16:25:04 -0600

    initial commit

---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private String nameOfTruck;
+	private String typeofFood;
+	private static int nextTruckId;
+	private int id;
+	
+	
+	
+	
+}
